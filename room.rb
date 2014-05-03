4cca88d315c88b7b403c7cdbe18515240d2dc765
Wilbert Uy Jr <wilbert@Wilberts-MacBook-Air.local>
2014-05-03 21:28:28 +0800

    updated

--- room.rb
+++ room.rb
@@ -9,14 +9,25 @@ class Room
 
 	def confirmation_details
 		if (@fname == "" || @lname == "" || @email == "" || @room == "default" || @date =="")
-			"Error! You must have left an entry blank."
+			"</br></br></br></br> 
+			Error! You must have left an entry blank."
+
 
 		else
-		"Hi #{@fname} #{@lname}. You chose our #{@room}.
-		You have booked on the date, #{@date}. For changes on your reservation, we can
-		contact you on #{@email}. Thank you for choosing MGM as your 
-		hotel for your vacation."
+				
+			"Hi #{@fname} #{@lname}. 
+			</br> 
+			Room: #{@room}
+			</br>
+			Date: #{@date} 
+			</br>
+			Contact Details: #{@email}
+			</br>
+			Thank you for choosing MGM as your 
+			hotel for your vacation."
 
 		end
 	end
+
+
 end
